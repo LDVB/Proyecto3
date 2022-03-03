305491f7b54b683081e95437cc489375c1b04173
Laura Del valle <l.delvallebermejo@gmail.com>
2022-03-03 15:21:38 +0100

    footer

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import './Navbar.css'
+
+const Navbar = () => {
+
+    return (
+
+
+        <body>
+            <nav class="navMenu">
+                <a href="#">Registrate</a>
+                <a href="#">Inicia sesión</a>
+                <div class="dot"></div>
+            </nav>
+        </body>
+
+
+    )
+
+}
+
+
+export default Navbar
